28e99261c88eee8ab64604283719b092bca6d833
Sandesh Rijal <sandesh.rijal@aayulogic.com>
2019-06-11 12:58:18 +0545

    Added functionality for creating users through API.

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,5 +1,21 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
 from . import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
+
+    def create(self, validated_data):
+        user = super(UserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class PostSerializer(serializers.ModelSerializer):
--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,7 @@ router.register('', views.PostViewSet)
 urlpatterns = [
     # path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('user-create/', views.UserCreateAPIView.as_view()),
 ]
 
 urlpatterns += router.urls
